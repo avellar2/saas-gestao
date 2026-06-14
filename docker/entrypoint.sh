7d1079a514f7080bc846939827a3aa3cfd4b5ef9
Vanderson Avellar <vandimavellar1997@gmail.com>
2026-06-14 02:14:01 -0300

    Fix Prisma migrate deploy command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-echo "⏳ DATABASE_URL=${DATABASE_URL}"
-echo "⏳ Running Prisma migrations..."
-./node_modules/.bin/prisma migrate deploy --url="${DATABASE_URL}"
+echo "DATABASE_URL=${DATABASE_URL}"
+echo "Running Prisma migrations..."
+./node_modules/.bin/prisma migrate deploy
 
-echo "🚀 Starting Next.js..."
+echo "Starting Next.js..."
 exec node server.js
