0218e643cdfc7d5ceeac8fe3842ebf2723811a5f
Vanderson Avellar <vandimavellar1997@gmail.com>
2026-06-14 02:00:59 -0300

    Fix Prisma Docker migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,12 +2,11 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
   migrations: {
     path: "prisma/migrations",
     seed: "npx tsx prisma/seed.ts",
-  },
-  async adapter() {
-    const { PrismaPg } = await import("@prisma/adapter-pg");
-    return new PrismaPg({ connectionString: env("DATABASE_URL") });
   },
 });
